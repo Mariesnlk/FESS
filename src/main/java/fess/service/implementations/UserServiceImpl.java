89bbdc150b0d57c6cfcc9e5db0dae8894ea3f25b
Mariesnlk <44849051+Mariesnlk@users.noreply.github.com>
2021-07-26 14:16:11 +0300

    add async transactions

---
+++ src/main/java/fess/service/implementations/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package fess.service.implementations;
+
+import fess.service.interfaces.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+
+}
+
